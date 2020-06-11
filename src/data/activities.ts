dbeb8544b7f51cb7379850ca16e997e6b3413736
Alex Owusu-Ansah <xan.ansah@gmail.com>
2020-06-11 23:25:38 +0100

    Convert Slack emoji index data into lists for food, drinks, and activities

---
+++ src/data/activities.ts
@@ -0,0 +1,7 @@
+import { Activity } from "../types"
+
+export const activities: Activity[] = [
+  { name: ":runner:", intensity: "Intense" },
+  { name: ":woman-golfing:", intensity: "Mild" },
+  { name: ":man-golfing:", intensity: "Mild" },
+]
