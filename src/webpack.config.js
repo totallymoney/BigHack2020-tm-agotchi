c602a3e058f3ea3c8cc8cab987b1f9e20090058e
“steve-westwood” <steve.westwood@gmail.com>
2020-06-11 17:45:41 +0100

    Intial serverless project

---
+++ src/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const slsw = require("serverless-webpack");
+
+module.exports = {
+  mode: slsw.lib.webpack.isLocal ? "development" : "production",
+  entry: slsw.lib.entries,
+  devtool: "source-map",
+  resolve: {
+    alias: {
+      "@common": path.resolve(__dirname, "../common/"),
+    },
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+  },
+  output: {
+    libraryTarget: "commonjs",
+    path: path.join(__dirname, ".webpack"),
+    filename: "[name].js",
+  },
+  target: "node",
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      {
+        test: /\.tsx?$/,
+        exclude: ["/node_modules/"],
+        loader: "ts-loader",
+      },
+    ],
+  },
+};
